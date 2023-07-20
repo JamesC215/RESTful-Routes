bad8e3d7ca3f605584eb98daf5b01fcf232fa7f2
JamesC215 <carter215@hotmail.co.uk>
2023-07-21 00:56:42 +0100

    revert previous changes 	modified:   .env 	modified:   config/database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,5 @@
 const mongoose = require('mongoose');
 
-// Assuming your environment variable is named MONGODB_URI
-const mongoURI = process.env.MONGODB_URI;
-
-// Connect to MongoDB
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
 mongoose.connect(process.env.DATABASE_URL);
 	
 const db = mongoose.connection;
